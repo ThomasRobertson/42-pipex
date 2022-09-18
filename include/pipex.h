aa7283b73090dea844be8b5d4cd3c9ea10cbbdf4
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-09-18 22:02:09 +0200

    refactor: reduce duplicate code

--- include/pipex.h
+++ include/pipex.h
@@ -6,12 +6,14 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 20:25:52 by troberts          #+#    #+#             */
-/*   Updated: 2022/09/03 20:23:42 by troberts         ###   ########.fr       */
+/*   Updated: 2022/09/18 22:00:57 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+# define _GNU_SOURCE 1
 
 # include "../libft/include/libft.h"
 
@@ -21,6 +23,7 @@
 # include <fcntl.h>
 # include <errno.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
 # include <sys/types.h>
 
 # define PIPE_READ 0
@@ -54,10 +57,9 @@ typedef struct s_split
 
 t_cmd	*get_path_of_cmd(char **envp, char *cmd_char);
 int		fork_and_execute_cmd(t_cmd **cmd_array, int fd_file[2]);
-t_cmd	**create_struct_cmd(int argc, char **argv, char **envp);
+t_cmd	**create_struct_cmd(int argc, char **argv, char **envp, int offset);
 
 int		here_doc(char *limiter);
-t_cmd	**create_struct_cmd_heredoc(char **argv, char **envp);
 
 void	free_cmd_array(t_cmd **cmd_array);
 void	free_cmd(t_cmd *cmd);
--- src/create_struct_cmd.c
+++ src/create_struct_cmd.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 22:36:08 by troberts          #+#    #+#             */
-/*   Updated: 2022/09/18 03:06:23 by troberts         ###   ########.fr       */
+/*   Updated: 2022/09/18 22:00:49 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,50 +43,24 @@ void	free_cmd_array(t_cmd **cmd_array)
 	free(cmd_array);
 }
 
-t_cmd	**create_struct_cmd_heredoc(char **argv, char **envp)
+t_cmd	**create_struct_cmd(int argc, char **argv, char **envp, int offset)
 {
-	int		i;
-	t_cmd	**cmd_array;
-
-	cmd_array = malloc(sizeof(*cmd_array) * (2 + 1));
-	if (cmd_array == NULL)
-		return (NULL);
-	cmd_array[2] = NULL;
-	i = 0;
-	while (i < 2)
-	{
-		cmd_array[i] = get_path_of_cmd(envp, argv[i + 3]);
-		if (cmd_array[i] == NULL)
-		{
-			free_cmd_array(cmd_array);
-			return (NULL);
-		}
-		i++;
-	}
-	return (cmd_array);
-}
-
-t_cmd	**create_struct_cmd(int argc, char **argv, char **envp)
-{
-	int		i;
 	int		j;
 	t_cmd	**cmd_array;
 
-	cmd_array = malloc(sizeof(*cmd_array) * (argc - 2));
+	cmd_array = malloc(sizeof(*cmd_array) * (argc - offset));
 	if (cmd_array == NULL)
 		return (NULL);
-	cmd_array[argc - 3] = NULL;
-	i = 2;
+	cmd_array[argc - offset - 1] = NULL;
 	j = 0;
-	while (i < (argc - 1))
+	while (j + offset < (argc - 1))
 	{
-		cmd_array[j] = get_path_of_cmd(envp, argv[i]);
+		cmd_array[j] = get_path_of_cmd(envp, argv[j + offset]);
 		if (cmd_array[j] == NULL)
 		{
 			free_cmd_array(cmd_array);
 			return (NULL);
 		}
-		i++;
 		j++;
 	}
 	return (cmd_array);
